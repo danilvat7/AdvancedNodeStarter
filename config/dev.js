b96aed3bf1f018fd66756dd4d74b41057079ce02
Daniyil Vatahovych <danilvat7@gmail.com>
2019-03-11 22:32:53 +0200

    deploy project

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb://developer:developer1@ds255924.mlab.com:55924/blog_dev',
+  cookieKey: '123123123'
+};
